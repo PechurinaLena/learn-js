9477c3fd7ad47885f3195c9561ce0c6e92b17afa
Elena Pechurina <www.waaay.net@gmail.com>
2019-06-07 14:54:35 +0300

    feat: lesson 2 done

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
-let money = "Ваш бюджет на месяц?";
-let time = "Введите дату в формате YYYY-MM-DD";
+let money = +prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-        "1000$": "800$",
-    },
+    expenses: {},
     income: [],
     savings: false
 };
 
-console.log("Введите обязательную статью расходов в этом месяце");
-console.log("Во сколько обойдется ?");
 
-alert(1000 / 30);
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце");
+    let b = prompt("Во сколько обойдется ?");
+
+    if ((typeof (a)) === "string" && (typeof (a)) != null &&
+        (typeof (b)) != null && a != "" && b != "" && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        alert("Произошла ошибка");
+    }
+}
+
+/*
+switch (a && b) {
+    case (typeof (a)) === "string":
+        console.log("done");
+        break;
+    case (typeof (a)) != null:
+        console.log("done");
+        break;
+    case (typeof (b)) != null:
+        console.log("done");
+        break;
+    case a != "" && b != "":
+        console.log("done");
+        break;
+    case a.length < 50:
+        console.log("done");
+        break;
+}
+*/
+
+appData.moneyPerDay = appData.budget / 30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимумальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
